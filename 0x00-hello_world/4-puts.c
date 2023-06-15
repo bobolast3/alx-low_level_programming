f560333ec55f8b632ec9ed8829268f6c8099740c
bobolast3 <bobolast3@gmail.com>
2023-06-15 09:30:59 -0700

    first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-int main() {
-
-    puts("Programming is like building a multilingual puzzle");
-	return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }

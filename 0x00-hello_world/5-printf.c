49962ae91f6639908ff4d8d8597c48c2af8dce08
bobolast3 <bobolast3@gmail.com>
2023-06-15 10:04:40 -0700

    first

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/**                                                                                                                                          
- * main - Entry point of the program                                                                                                         
- *                                                                                                                                           
- * Description: Prints a message using puts()                                                                                                
- *                                                                                                                                           
- * Return: Always 0 (Success)                                                                                                                
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Programming is like building a multilingual puzzle\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }

849586a30f961f6b728545976b47a5c2f30c56a4
bobolast3 <bobolast3@gmail.com>
2023-06-17 08:12:39 -0700

    first

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+for (int i = 0; i < 26; i++)
+{
+putchar(alphabet[i]);
+putchar('\n');
+}
+return (0);
+}

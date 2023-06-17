c8375eff901f91e30d558cee1bb91db01c3045f9
bobolast3 <bobolast3@gmail.com>
2023-06-17 07:09:42 -0700

    first

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,16 +11,20 @@
 
 int main(void)
 {
-  int n;
-  n = rand() - RAND_MAX / 2;
-  if (n < 0){
-    printf("%d is negative", n);
-  }
-  else if (n == 0){
-    printf("%d is zero", n);
-  }
-  else if (n > 0){
-    printf("%d is positive", n);
-  }
-  return (0);
+int n;
+ 
+n = rand() - RAND_MAX / 2;
+if (n < 0)
+{
+printf("%d is negative ", n);
+}
+else if (n == 0)
+{
+printf("%d is zero ", n);
+}
+else if (n > 0)
+{
+printf("%d is positive ", n);
+}
+return (0);
 }

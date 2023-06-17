df03f2b841b5cad9de046063eee127eac9127c01
bobolast3 <bobolast3@gmail.com>
2023-06-17 09:57:36 -0700

    first

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = 0;
+while (c <= 'f')
+{
+if (c < 10)
+{
+putchar(c + '0');
+}
+else
+{
+putchar(c);
+}
+c++;
+if (c == 10){
+c='a';
+}
+}
+putchar('\n');
+return (0);
+}

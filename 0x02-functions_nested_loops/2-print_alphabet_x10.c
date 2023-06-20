ffd11bf3ef073307be8613c4275d758e98ddf2c8
bobolast3 <bobolast3@gmail.com>
2023-06-20 09:16:39 -0700

    first

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_alphabet_x10 - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+while(i < 10)
+{
+print_alphabet();
+_putchar('\n');
+i++;
+}
+}

f908b422f1c940154896231dbd959af2563788ff
bobolast3 <bobolast3@gmail.com>
2023-06-20 10:16:00 -0700

    first

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _islower - Entry point of the program
+ * int _islower(int c) - Entry point of the program
  *
  * Description: Prints a message using puts()
  *
  * Return: Always 0 (Success)
  */
-int _islower(int c) {
-if (c >= 97 && c <= 122){
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
 return 1;
 }
-else {
 return (0);
-}
 }

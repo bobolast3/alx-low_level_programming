bb9a89f91fe31aa417ce49d6c46140b7226c8cc6
bobolast3 <bobolast3@gmail.com>
2023-06-20 14:50:08 -0700

    first

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,31 @@
 #include "main.h"
 /**
  * times_table - Checks if a number is a single digit.
- * @num: The number to check.
+ * 
  *
  * Return: 1 if the number is a single digit, 0 otherwise.
  */
 void times_table(void) {
 int i = 0;
 int j = 0;
-while (i < 10) {
-while (j < 10) {
+while (i < 10) 
+{
+while (j < 10) 
+{
 int n = i * j;
 if (n < 10)
 {
+_putchar(' ');
 _putchar(n + '0');
 }
-else {
+else 
+{
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 }
 if (j < 9)
-{
 _putchar(',');
 _putchar(' ');
-if (i * j < 10)
-{
-_putchar(' ');
-}
-}
 j++;
 }
 _putchar('\n');

a1e8ce0d262043fec82c68af6fd11109d0f26a0e
bobolast3 <bobolast3@gmail.com>
2023-06-20 06:57:21 -0700

    first

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+
+int i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+putchar("\n");
+return (0);
+}

a433ac5d0744b7ddfb112bb050c035e5dfaa4dde
bobolast3 <bobolast3@gmail.com>
2023-06-20 07:31:43 -0700

    first

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - Entry point of the program
+ * print_alphabet - something
  *
  * Description: Prints a message using puts()
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
-/*description*/
 {
 int i = 'a';
 while (i <= 'z')

d5fcb2a6123ab76f735000a71a6c45809cfd5073
bobolast3 <bobolast3@gmail.com>
2023-06-20 15:19:15 -0700

    first

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,24 +8,32 @@
 void times_table(void) {
 int i = 0;
 int j = 0;
-while (i < 10) 
+while (i < 10)
 {
-while (j < 10) 
+while (j < 10)
 {
 int n = i * j;
 if (n < 10)
 {
+if (j > 0)
+{
 _putchar(' ');
+if (i == 0 && j > 1)
+_putchar(' ');
+}
 _putchar(n + '0');
 }
-else 
+else
 {
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 }
 if (j < 9)
 _putchar(',');
+if (i > 0 && j > 0)
+{
 _putchar(' ');
+}
 j++;
 }
 _putchar('\n');

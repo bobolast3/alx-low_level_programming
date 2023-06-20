a2ac8e0951876e09507ccd5027ccc661f43577d5
bobolast3 <bobolast3@gmail.com>
2023-06-20 06:24:08 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message using puts()
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}

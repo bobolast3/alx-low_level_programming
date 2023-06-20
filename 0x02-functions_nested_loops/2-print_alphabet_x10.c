9f195dcc626a791d9e41daf35c9c7afe7f3380d1
bobolast3 <bobolast3@gmail.com>
2023-06-20 09:29:29 -0700

    first

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,16 @@
  */
 void print_alphabet_x10(void)
 {
-int i = 0;
-while(i < 10)
+int j = 0;
+while(j < 10)
 {
-print_alphabet();
-_putchar('\n');
+int i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
 i++;
+}
+_putchar('\n');
+j++;
 }
 }

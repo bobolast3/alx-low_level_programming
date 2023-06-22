06debfe7ee9ba685357405f1b8752c47d8e6e877
bobolast3 <bobolast3@gmail.com>
2023-06-22 14:37:53 -0700

    first

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
@@ -11,20 +11,25 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
-} 
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
 return (0);
 }

0cf99f03531973e8e83556590d05763583a6c038
bobolast3 <bobolast3@gmail.com>
2023-06-22 12:58:00 -0700

    first

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,6 @@
 /**
  * _isdigit - checks if a character is a digit (0-9)
  * @c: character to be checked
- *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
@@ -10,5 +9,5 @@ int _isdigit(int c)
 if (c >= '0' && c <= '9')
 return (1); 
 else
-return 0;
+return (0);
 }

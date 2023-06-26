9ca7446a2427cfbdddae54fea18a3c1a0f3d77dd
bobolast3 <bobolast3@gmail.com>
2023-06-26 13:55:15 -0700

    first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * puts2 - Prints every other character of a string, 
+ * puts2 - Prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: Pointer to the string.
  *
  * This function takes a pointer to a string and prints every other character,
- * starting with the first character, followed by a new line to the 
+ * starting with the first character, followed by a new line to the
  * standard output (stdout).
  *
  * Return: None.
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * puts_half - Prints the second half of a string followed by a new line.
+ * @str: Pointer to the string.
+ *
+ * This function takes a pointer to a string and prints the second half
+ * of the string followed by a new line to the standard output (stdout).
+ * If the number of characters is odd, it prints the last n characters
+ * of the string, where n = (length_of_the_string - 1) / 2.
+ *
+ * Return: None.
+ */
+void puts_half(char *str)
+{
+int length = 0;
+int i;
+if (str == NULL)
+return;
+while (str[length] != '\0')
+length++;
+int start_index;
+if (length % 2 == 0)
+{
+start_index = length / 2;
+} 
+else
+{
+start_index = (length - 1) / 2;
+}
+i = start_index;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n'); 
+}

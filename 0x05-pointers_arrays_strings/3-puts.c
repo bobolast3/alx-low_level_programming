878d8d683ed672453dd7feec748d36374e2599da
bobolast3 <bobolast3@gmail.com>
2023-06-26 12:41:52 -0700

    first

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string.
+ *
+ * This function takes a pointer to a string and prints the string
+ * followed by a new line to the standard output (stdout).
+ *
+ * Return: None.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+}

c3dc6da1de64d9c21c1f8e31e45e418382b958d8
bobolast3 <bobolast3@gmail.com>
2023-06-26 15:27:51 -0700

    first

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
+ * print_array - Prints n elements of an array
+ * of integers, followed by a new line.
  * @a: Pointer to the array of integers.
  * @n: Number of elements to be printed.
  *
- * This function takes a pointer to an array of integers and the number of elements `n`,
- * and prints the elements of the array followed by a new line. The numbers are displayed
- * in the same order as they are stored in the array, separated by a comma and a space.
+ * This function takes a pointer to an array
+ * of integers and the number of elements `n`,
+ * and prints the elements of the array followed
+ * by a new line. The numbers are displayed
+ * in the same order as they are stored in
+ * the array, separated by a comma and a space.
  *
  * Return: None.
  */

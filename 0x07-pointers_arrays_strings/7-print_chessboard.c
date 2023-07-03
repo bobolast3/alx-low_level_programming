a238d624fff8312634880331959d4f09502ded09
bobolast3 <bobolast3@gmail.com>
2023-07-03 10:54:46 -0700

    first

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: 2D array representing the chessboard.
+ *
+ * This function prints the chessboard using the provided 2D array.
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
+}

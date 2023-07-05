3b7a16680061722940011462013eeb75ffc16921
bobolast3 <bobolast3@gmail.com>
2023-07-05 09:14:49 -0700

    first

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,15 +28,16 @@ return (sqrt_helper(n, 1, n));
  */
 int sqrt_helper(int n, int start, int end)
 {
-int mid;
+int mid, sqrt;
 if (start <= end)
 {
 mid = (start + end) / 2;
-if (mid * mid == n)
+sqrt = n / mid;
+if (sqrt == mid && n % mid == 0)
 return (mid);
-if (mid * mid < n)
-return (sqrt_helper(n, mid + 1, end));
+if (sqrt < mid)
 return (sqrt_helper(n, start, mid - 1));
+return (sqrt_helper(n, mid + 1, end));
 }
 return (end);
 }

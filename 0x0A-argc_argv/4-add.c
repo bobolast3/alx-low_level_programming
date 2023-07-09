fa8f1515e3bf9efa1a30a53b34efc5ca80a747d2
bobolast3 <bobolast3@gmail.com>
2023-07-09 16:21:38 -0700

    first

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,23 +8,29 @@
  *
  * Return: Always 0 or 1 (success)
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 int sum = 0;
 int i, num;
-if (argc < 2) {
+if (argc < 2)
+{
 printf("0\n");
 return (0);
 }
-for (i = 1; i < argc; i++) {
+for (i = 1; i < argc; i++)
+{
 int j;
-for (j = 0; argv[i][j] != '\0'; j++) {
-if (!isdigit(argv[i][j])) {
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
 printf("Error\n");
 return (1);
 }
 }
 num = atoi(argv[i]);
-if (num > 0) {
+if (num > 0)
+{
 sum += num;
 }
 }

cbfebf37cbfddb538b5d5e3fbe34dc873ad52a4d
bobolast3 <bobolast3@gmail.com>
2023-07-17 09:21:40 -0700

    first

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,6 +12,15 @@
  *
  * Return: None
  */
+/**
+ * struct dog - Represents information about a dog
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to the name of the owner
+ *
+ * Description: This structure holds information about a dog, including its name,
+ *              age, and the name of its owner.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 d->name = name;

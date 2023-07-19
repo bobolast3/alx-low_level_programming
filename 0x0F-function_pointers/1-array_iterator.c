90ac30ffcd753c880d32216326b0a5c1ca5e95cf
bobolast3 <bobolast3@gmail.com>
2023-07-19 14:25:28 -0700

    first

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,9 +16,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 if (array != NULL && action != NULL)
 {
-for (size_t i = 0; i < size; i++)
+size_t i = 0;
+while (i < size)
 {
 action(array[i]);
+i++;
 }
 }
 }

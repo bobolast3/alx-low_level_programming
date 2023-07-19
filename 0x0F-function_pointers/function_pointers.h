8d4c3392cf81e65eab02a9685a5ce6d57b91fa81
bobolast3 <bobolast3@gmail.com>
2023-07-19 11:34:32 -0700

    first

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
 #include <stddef.h>
+
+/* Function Prototypes */
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 typedef void (*action_func)(int);
-void array_iterator(int *array, size_t size, action_func action);
 void print_elem(int elem);
 void print_elem_hex(int elem);
 typedef int (*cmp_func)(int);
@@ -14,4 +16,5 @@ int int_index(int *array, int size, cmp_func cmp);
 int is_98(int elem);
 int is_strictly_positive(int elem);
 int abs_is_98(int elem);
+
 #endif /* FUNCTION_POINTERS_H */

88920447f2689ce9cbbed17525b2db02e842dbbf
bobolast3 <bobolast3@gmail.com>
2023-07-19 11:26:56 -0700

    first

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - Searches for an integer in
+ * an array using a comparison function
+ * @array: Pointer to the integer array
+ * @size: Number of elements in the array
+ * @cmp: Pointer to the comparison function
+ *
+ * Return: Index of the first element for
+ * which cmp doesn't return 0, -1 if not found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+if (array != NULL && cmp != NULL && size > 0)
+{
+for (int i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+}
+}
+return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
-/* Function Prototypes */
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
@@ -10,4 +9,9 @@ typedef void (*action_func)(int);
 void array_iterator(int *array, size_t size, action_func action);
 void print_elem(int elem);
 void print_elem_hex(int elem);
+typedef int (*cmp_func)(int);
+int int_index(int *array, int size, cmp_func cmp);
+int is_98(int elem);
+int is_strictly_positive(int elem);
+int abs_is_98(int elem);
 #endif /* FUNCTION_POINTERS_H */

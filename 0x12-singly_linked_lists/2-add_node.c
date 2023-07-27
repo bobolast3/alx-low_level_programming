ae5882aa3950b2ec0a1ca474121fd73b2ef83418
bobolast3 <bobolast3@gmail.com>
2023-07-27 07:29:59 -0700

    first

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,10 +11,10 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node = malloc(sizeof(list_t));
+
 	if (str == NULL)
 		return (NULL);
-
-	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -22,8 +22,8 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-		free(new_node)
-			return (NULL);
+		free(new_node);
+		return (NULL);
 	}
 
 	new_node->len = strlen(str);

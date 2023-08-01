89e7cbe3da607f07e48bd4f3ca6c24d893d6d719
bobolast3 <bobolast3@gmail.com>
2023-08-01 04:33:17 -0700

    first

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
-	return;
-
 	listint_t *current = *head;
 	listint_t *next;
+
+	if (head == NULL)
+	return;
 
 	while (current != NULL)
 	{
